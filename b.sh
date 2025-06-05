741491e106c63b4f2b6a914520d1583313e29133
Ron Gurr <ron.gurr@microsoft.com>
2025-06-04 17:59:07 -0700

    Minor cleanup, removed unused feature and relying on default dev profile

--- b.sh
+++ b.sh
@@ -1,2 +1,2 @@
 export RUSTC_BOOTSTRAP=1
-cargo build --target aarch64-unknown-uefi --features aarch64 --profile dev -Zbuild-std=core,compiler_builtins,alloc -Zbuild-std-features=compiler-builtins-mem -Zunstable-options --timings=html
+cargo build --target aarch64-unknown-uefi -Zbuild-std=core,compiler_builtins,alloc -Zbuild-std-features=compiler-builtins-mem -Zunstable-options --timings=html
