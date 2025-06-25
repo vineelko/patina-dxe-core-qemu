073ee680a0d1f761d9f0d227e4c6ce28cbd1fc4e
Ron Gurr <ron.gurr@microsoft.com>
2025-06-25 16:29:14 -0700

    Quick local build updates

--- b.sh
+++ b.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export RUSTC_BOOTSTRAP=1
 export RUST_BACKTRACE=full
-cargo build --target aarch64-unknown-uefi -Zbuild-std=core,compiler_builtins,alloc -Zbuild-std-features=compiler-builtins-mem -Zunstable-options --timings=html
+cargo build --profile dev --target aarch64-unknown-uefi -Zbuild-std=core,compiler_builtins,alloc -Zbuild-std-features=compiler-builtins-mem -Zunstable-options --timings=html
